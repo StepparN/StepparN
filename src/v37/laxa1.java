0f96912c5506ab69125464d4a05f33659ef7e65e
StepparN <kalle.favoryt@elev.ga.ntig.se>
2019-09-16 12:06:19 +0200

    Update laxa1.java

--- src/v37/laxa1.java
+++ src/v37/laxa1.java
@@ -1,44 +1,10 @@
 package v37;
 
-import java.util.Scanner;
-
 public class laxa1 {
 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
-        System.out.println("Vad heter du?");
-
-        Scanner namnInput = new Scanner (System.in);
-        String namn = namnInput.nextLine();
-
-        System.out.println("Hur gammal är du?");
-
-        Scanner ålderInput = new Scanner (System.in);
-        String ålder = namnInput.nextLine();
-
-        System.out.println("Vad är din adress?");
-
-        Scanner adressInput = new Scanner (System.in);
-        String adress = adressInput.nextLine();
-
-        System.out.println("Vad är ditt postnummer?");
-
-        Scanner postnummerInput = new Scanner (System.in);
-        String postnummer = postnummerInput.nextLine();
-        
-        System.out.println("Vad är ditt telefonnummer?");
-
-        Scanner telefonnummerInput = new Scanner (System.in);
-        String telefonnummer = adressInput.nextLine();
-
-        System.out.println("Information" + "\n");
-        System.out.println("Namn:Kalle" + "\n");
-        System.out.println("Ålder:17" + "\n");
-        System.out.println("Adress:Övägen 123" + "\n");
-        System.out.println("Postnummer:12345" + "\n");
-        System.out.println("Telefonnummer:0123456789" + "\n");
-
-    }
+	}
 
 }
