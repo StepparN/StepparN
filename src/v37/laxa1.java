7fa41da78b6c89909dd9cb7f47197fb776d5624d
StepparN <kalle.favoryt@elev.ga.ntig.se>
2019-09-16 12:38:11 +0200

    Update laxa1.java

--- src/v37/laxa1.java
+++ src/v37/laxa1.java
@@ -23,6 +23,27 @@ public class laxa1 {
 		System.out.println("Vilken stad bor du i?");
 		String stad = input.nextLine();
 		
+		System.out.println("Vad ‰r ditt telefonnummer?");
+		String telefonnummer = input.nextLine();
+		
+		System.out.println("\nInformation:");
+		System.out.println("\nNamn:");
+		System.out.println(namn1);
+		
+		System.out.println("\n≈lder:");
+		System.out.println(alder);
+		
+		System.out.println("\nAdress:");
+		System.out.println(adress);
+		
+		System.out.println("\nPostnummer:");
+		System.out.println(postnummer);
+		
+		System.out.println("\nStad:");
+		System.out.println(stad);
+		
+		System.out.println("\nTelefeonnummer:");
+		System.out.println(telefonnummer);
 		
 	}
 
