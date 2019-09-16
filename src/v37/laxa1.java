c7b285d70f2f06235481d34c4d57776b9ca2a78f
StepparN <kalle.favoryt@elev.ga.ntig.se>
2019-09-16 12:14:39 +0200

    Update laxa1.java

--- src/v37/laxa1.java
+++ src/v37/laxa1.java
@@ -7,6 +7,11 @@ public class laxa1 {
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Vad heter du?");
+		
+		String namn1 = input.nextLine();
+		
 	}
 
 }
