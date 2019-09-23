4885306c764feb5220e6044fe042aa5e6be6dbb8
StepparN <kalle.favoryt@elev.ga.ntig.se>
2019-09-23 12:23:25 +0200

    läxa2

---
+++ src/v38/laxa2.java
@@ -0,0 +1,21 @@
+package v38;
+
+import java.util.Scanner;
+
+public class laxa2 {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Skriv in 5 heltal");
+		String tal1 = input.nextLine();
+		String tal2 = input.nextLine();
+		String tal3 = input.nextLine();
+		String tal4 = input.nextLine();
+		String tal5 = input.nextLine();
+		
+
+	}
+
+}
