5be6f818af69e2ae3e61cd00723934c27c18418d
StepparN <kalle.favoryt@elev.ga.ntig.se>
2019-09-23 13:14:14 +0200

    Update laxa2.java

--- src/v38/laxa2.java
+++ src/v38/laxa2.java
@@ -21,6 +21,9 @@ public class laxa2 {
 		tal4 = input.nextInt();
 		tal5 = input.nextInt();
 		
+		double medel = (tal1 + tal2 + tal3 + tal4 + tal5);
+		double medel2 = (medel)/5;
+		
 		if (tal1 >=tal2 & tal1 >=tal3 & tal1 >=tal4 & tal1 >=tal5) {
 		System.out.println("Högsta talet:" + tal1);
 		}
@@ -36,7 +39,25 @@ public class laxa2 {
 		else if (tal5 >=tal1 & tal5 >=tal2 & tal5 >=tal3 & tal5 >=tal4) {
 		System.out.println("Högsta talet:" + tal5);
 		}
-
+		
+		
+		if (tal1 <=tal2 & tal1 <=tal3 & tal1 <=tal4 & tal1 <=tal5) {
+		System.out.println("Lägsta talet:" + tal1);
+		}
+		else if (tal2 <=tal1 & tal2 <=tal3 & tal2 <=tal4 & tal2 <=tal5) {
+		System.out.println("Lägsta talet:" + tal1);
+		}
+		else if (tal3 <=tal1 & tal3 <=tal2 & tal3 <=tal4 & tal3 <=tal5) {
+		System.out.println("Lägsta talet:" + tal1);
+			}
+		else if (tal4 <=tal1 & tal4 <=tal2 & tal4 <=tal3 & tal4 <=tal5) {
+		System.out.println("Lägsta talet:" + tal1);
+		}
+		else if (tal5 <=tal1 & tal5 <=tal2 & tal5 <=tal3 & tal5 <=tal4) {
+		System.out.println("Lägsta talet:" + tal1);
+		}
+		
+		System.out.println("Medelvärde:" + medel2);
 		
 		}
 		
