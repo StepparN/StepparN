fcba921e5d5b2f86279d22276bea554d11e49023
StepparN <kalle.favoryt@elev.ga.ntig.se>
2019-09-24 11:37:07 +0200

    Update lektionsuppgifter.java

--- src/v39/lektionsuppgifter.java
+++ src/v39/lektionsuppgifter.java
@@ -5,6 +5,16 @@ public class lektionsuppgifter {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int sum = 0; // initierar en summa
+		
+		for ( int i = 0 ; i < 100 ; i++) { // kör en loop 100 gånger
+			
+			sum = sum + i; 	//adderar föregående summan med i
+			
+		}
+		
+		System.out.println(sum); // skriver ut totala summan
+		
 	}
 
 }
