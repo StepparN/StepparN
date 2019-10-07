e8b48cff5040d8f90493dde63976ee896d757aa8
StepparN <kalle.favoryt@elev.ga.ntig.se>
2019-10-07 12:45:36 +0200

    fizzbuzz

--- src/Kattis/fizzbuzz.java
+++ src/Kattis/fizzbuzz.java
@@ -1,10 +1,27 @@
 package Kattis;
 
+import java.util.Scanner; 
+
 public class fizzbuzz {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		int n = sc.nextInt();
+		
+		for (int i = 1; i <= n; i++) {
+			if (i % x == 0 && i % y == 0) {
+				System.out.println("Fizzbuzz");
+				
+			}else if (i % x == 0) {
+				System.out.println("Fizz");
+				
+			}else if (i % y == 0) {
+				
+			}
 
-	}
+		}
 
-}
+
--- v41/lektion.java
+++ v41/lektion.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
 
 public class lektion {
 
+	
 	public static void main(String[] args) {
 		
-		
+	Scanner input = new Scanner(System.in);
+	
+	int tal1 = 0;
+	int tal2 = 0;
+	
+	System.out.println("Skriv ut heltal");
+	
+	tal1 = input.nextInt();
+	tal2 = input.nextInt();
+	
+	if (tal1 >= tal2) {
+		System.out.println("Lägsta numret");
+	}
+	else {
+		System.out.println("Lägsta numret" + tal2);
+	
+	}
 		
 	}
 }
