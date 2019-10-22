47d71e4dbae29394551053e925f8619570d7399e
StepparN <kalle.favoryt@elev.ga.ntig.se>
2019-10-22 12:06:21 +0200

    Create laxa5.java

---
+++ v43/laxa5.java
@@ -0,0 +1,23 @@
+
+public class laxa5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		lol();
+		max(3,4);
+
+	}
+	
+	public static void lol() {
+		System.out.println("HAHAHAHAHAHA");
+	}
+	
+	public static void max(int tal1, int tal2) {
+		
+		
+	}
+	
+	}
+
+
