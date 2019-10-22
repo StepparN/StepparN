058c99230715cf11e0b59ea1efb98908636ce1fc
StepparN <kalle.favoryt@elev.ga.ntig.se>
2019-10-22 11:41:14 +0200

    lektion100

---
+++ v43/metoder.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class metoder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int tal1;
+		int tal2;
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Skriv in 2 tal med enter emellan");
+		tal1 = input.nextInt();
+		tal2 = input.nextInt();
+		
+		if (tal1 <= tal2) {
+			System.out.println("Minsta talet är:" + tal1);
+		}
+		else {
+			System.out.println("Minsta talet är:" + tal2);
+		}
+
+		
+		
+	}
+
+}
