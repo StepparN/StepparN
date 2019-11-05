530092273c4d44888d51df126cf378893d29b6d3
StepparN <kalle.favoryt@elev.ga.ntig.se>
2019-11-05 12:05:45 +0100

    math

---
+++ v45/mastring.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class mastring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double svar = Math.sqrt((Math.pow(3, 2) + 4));
+		System.out.println(svar);
+		
+		Math.max(3, 7);
+		
+		String mening = "Tomas är dum men taskig!";
+		System.out.println(mening);
+		
+		Scanner input = new Scanner(System.in);
+	}
+/*		String mening2 = input.nextLine();
+		
+		if (mening.equals(mening2)) {
+			System.out.println("Samma mening");
+		}
+	*/	
+		System.out.println(mening.length());
+		System.out.println(mening.toUpperCase());
+				
+
+	
+	}
+
+
