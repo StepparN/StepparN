3300210b6afef465c0416f1f9f307982cba335e1
StepparN <kalle.favoryt@elev.ga.ntig.se>
2019-11-12 11:59:43 +0100

    Update MethodLibrary.java

--- v46/MethodLibrary.java
+++ v46/MethodLibrary.java
@@ -4,8 +4,41 @@ public class MethodLibrary {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		System.out.println((fahrenheitToKelvin(5)));
 		
+		System.out.println((KelvinToCelsius(100)));
+		
+		System.out.println((velocityConversion(100)));
+		
+	   }
+/**
+ * Omvandlar Fahrenheit till Kelvin.
+ * @param fahrenheit
+ * @return
+ */
+    public static double fahrenheitToKelvin(double fahrenheit) {
+        return  (fahrenheit + 459.67) * 5/9;
+    }
+/**
+ * Omvandlar Kelvin till Celsius.
+ * @param Kelvin
+ * @return
+ */
+    public static double KelvinToCelsius(double Kelvin) {
+        return Kelvin-273.15;
+    }
+/**
+ * Omvandlar km/h till m/s.
+ * @param velocityKmH
+ * @return
+ */
+    public static double velocityConversion(double velocityKmH) {
+        return (velocityKmH)/3.6;
 
+    public static double kineticEnergy(double mass, double velocity) {
+        
+    }
+            
 	}
 
 }
