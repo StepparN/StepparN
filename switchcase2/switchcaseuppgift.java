be14aee72e25881bcdccb0a19bfa76ef80e0613b
StepparN <kalle.favoryt@elev.ga.ntig.se>
2019-12-17 11:42:40 +0100

    Create switchcaseuppgift.java

---
+++ switchcase2/switchcaseuppgift.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class switchcaseuppgift {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		
+		int choice;
+		
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		
+		choice = input.nextInt();
+
+		switch (choice) {
+		case 1:
+			System.out.println("");
+			break;
+
+		case 2:
+			System.out.println("");
+			break;
+
+		case 3:
+			System.out.println("");
+			break;
+
+		case 4:
+			System.out.println("");
+			break;
+
+		case 5:
+			System.out.println("");
+			break;
+			
+		default:
+			System.out.println("Välj en siffra 1-5...");
+			break;
+
+		}
+	}
+}
+
