04a9b43831e8e25aff23965e0f5988eb6d5ea12f
StepparN <kalle.favoryt@elev.ga.ntig.se>
2020-01-13 12:34:40 +0100

    bubblesort

---
+++ w3/bubbleSort.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class bubbleSort {
+
+	public static void main(String[] args) {
+
+		int[] numbers = { 1, 2, 5, 3, 5, 7, 0, 4, 3, 6, 9, 7, 10 };
+		boolean swapped;
+		int temp;
+		
+		System.out.println(Arrays.toString(numbers));
+		
+		do {
+
+			swapped = false; // nollställer swapped i början på jämförelsen
+			
+			for (int i = 0; i < numbers.length - 1; i++) {
+				if (numbers[i] > numbers[i + 1]) { // byter plats på två intilliggande tal
+					temp = numbers[i]; // sparar tillfälligt värdet på vänstra sidan
+					numbers[i] = numbers[i + 1];
+					numbers[i + 1] = temp;
+					swapped = true;
+					System.out.println(Arrays.toString(numbers));
+				}
+
+			}
+		} while (swapped);
+		
+		System.out.println(Arrays.toString(numbers));
+
+	}
+}
